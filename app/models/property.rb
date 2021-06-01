17d7055d09472f9ff1561d36de0400da6f772ebc
Sadhvi Tripathi <sadhvi.tripathi23@gmail.com>
2021-06-01 21:45:24 +0530

    Handled errors in API structure and added validations

--- app/models/property.rb
+++ app/models/property.rb
@@ -3,19 +3,15 @@ class Property < ApplicationRecord
   # Associations
   belongs_to :user, foreign_key: 'owner_id', optional: true
 
+  # Validation
+  validates :owner_id, presence: true
+  validates :name, presence: true
+
   # Callbacks
   before_update :check_approval_status, if: :is_approved_changed?
-  before_update :check_admin_can_update
 
   # Check only owner can update the approval status
   def check_approval_status
     raise "User can't approve/reject properties" if user&.role&.name == 'user'
-  end
-
-  # Validate that user and admin cant update other property attributes
-  def check_admin_can_update
-    if (user&.role&.name == 'user' || user&.role&.name == 'admin') && (is_available_changed? || name_changed? || is_active_changed?)
-      raise "Admin or User can't update other property attributes"
-    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,14 @@ Rails.application.routes.draw do
   # properties routes
   get '/properties', to: 'properties#index'
   post '/properties', to: 'properties#create'
+  get '/properties/me', to: 'properties#my_properties'
   get '/properties/:id', to: 'properties#show'
   put '/properties/:id', to: 'properties#update'
   delete '/properties/:id', to: 'properties#destroy'
   put '/properties/:id/approval_status', to: 'properties#update_approval_status'
   put '/properties/:id/availability', to: 'properties#update_availability'
   put '/properties/:id/deactivate', to: 'properties#deactivate'
-  get '/properties/me', to: 'properties#my_properties'
+
 
   # users routes
   resources :users
